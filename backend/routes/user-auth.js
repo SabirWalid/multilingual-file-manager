89e54ea1ff1f789f53844c75a5728bb9b0b2e390
SabirWalid <s.abdurahma@alustudent.com>
2024-11-30 00:58:36 +0200

    backend

--- backend/routes/user-auth.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as userAuthControler from "../controllers/user-auth.js";
-
-const router = Router();
-
-router.post("/login", userAuthControler.login);
-
-export default router;
